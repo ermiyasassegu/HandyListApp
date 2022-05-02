d518016196f791d553d7e753eeb14df46a211cc4
ermiyasassegu <67770426+ermiyasassegu@users.noreply.github.com>
2022-05-02 13:20:54 +0300

    Setting darkmode part is ready

--- HandyListApp/Views/Main/SettingView.swift
+++ HandyListApp/Views/Main/SettingView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State private var darkMode:Bool = false
+    @State private var currentMode:ColorScheme = .light
     var body: some View {
-        Text("V.0.0.1")
+        NavigationView {
+            Form {
+                Toggle("Dark Mode", isOn: $darkMode)
+                    .onChange(of: darkMode) { value in
+                        if darkMode == true {
+                            currentMode = .dark
+                        } else {
+                            currentMode = .light
+                        }
+                        
+                    }
+            }.navigationTitle("Settings")
+                .preferredColorScheme(currentMode)
+                .toggleStyle(SwitchToggleStyle(tint: Color.red))
+        }
     }
 }
 
